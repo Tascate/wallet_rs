b1821f76e258c17fbb66804888ebe155970ec083
Troy <tvinsixiengmay@gmail.com>
2021-11-18 14:35:52 -0800

    Added select pay screen

---
+++ lib/select_pay.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+
+class selectPay extends StatelessWidget {
+  const selectPay({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: ListView(
+        children: [
+          TextField(
+            decoration: const InputDecoration(
+              border: OutlineInputBorder(),
+              hintText: 'Search'
+            ),
+          ),
+          Column(
+            children: [
+              Row(
+                children: [
+                  Column(
+                    children: [
+                      Placeholder(
+                        
+                      ),
+                      Text("Name")
+                    ],
+                  ),
+                  Column(
+                    children: [
+                      Placeholder(
+                        
+                      ),
+                      Text("Name")
+                    ],
+                  ),
+                  Column(
+                    children: [
+                      Placeholder(
+                        
+                      ),
+                      Text("Name")
+                    ],
+                  )
+                ],
+              )
+            ],
+          )
+        ],
+      ),
+    )
+  }
+}
