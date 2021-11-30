f91c51e2ec108bf4155c48f49be9c64122d58928
Troy <tvinsixiengmay@gmail.com>
2021-11-29 17:24:25 -0800

    Made Transaction Comparable for Sorting

--- lib/transaction.dart
+++ lib/transaction.dart
@@ -1,8 +1,39 @@
-class Transaction {
+import 'dart:collection';
+import 'package:flutter/cupertino.dart';
+
+class Transaction implements Comparable<Transaction> {
   double money;
   String title;
   DateTime timestamp;
   bool isSpending;
 
   Transaction(this.money, this.isSpending, this.title, this.timestamp);
+
+  @override
+  int compareTo(Transaction rhs) => timestamp.compareTo(rhs.timestamp);
+}
+
+class TransactionModel extends ChangeNotifier {
+  final List<Transaction> transactions = [];
+
+  //Get unmodifiable view of transaction list
+  UnmodifiableListView<Transaction> get items =>
+      UnmodifiableListView(transactions);
+
+  //length of transactions
+  int get numTransactions => transactions.length;
+
+  void addTransaction(
+      double money, String title, DateTime timestamp, bool isSpending) {
+    var newTransaction = Transaction(money, isSpending, title, timestamp);
+    transactions.add(newTransaction);
+    transactions.sort();
+    notifyListeners();
+  }
+
+  void quickAdd(Transaction newTransaction) {
+    transactions.add(newTransaction);
+    transactions.sort();
+    notifyListeners();
+  }
 }
